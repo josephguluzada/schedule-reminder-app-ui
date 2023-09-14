153f253da91fed8b64cdf3b79f10d0e4c8bbb7b8
josephguluzada <yquluzade98@gmail.com>
2023-09-14 16:28:19 +0400

    feat: added yup formik validator

--- schedule-reminder/src/components/Form.jsx
+++ schedule-reminder/src/components/Form.jsx
@@ -1,87 +1,116 @@
-import React, { useState } from "react";
 import axios from "axios";
 import "./Form.css";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+
+const validationSchema = Yup.object().shape({
+  to: Yup.string().required("To field is required"),
+  method: Yup.string().required("Method field is required"),
+  sendAt: Yup.date()
+    .required("Send at date is required")
+    .min(new Date(), "Send at date must be in the future"),
+  content: Yup.string().required("Content field is required"),
+});
 
 function Form() {
   const url = "https://localhost:7075/api/Reminders";
-  const [data, setData] = useState({
-    to: "",
-    method: "",
-    sendAt: "",
-    content: "",
+
+  const formik = useFormik({
+    initialValues: {
+      to: "",
+      method: "email",
+      sendAt: "",
+      content: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      const formData = {
+        to: values.to,
+        method: values.method,
+        sendAt: new Date(values.sendAt).toISOString(),
+        content: values.content,
+      };
+      axios.post(url, formData).then((response) => console.log(response));
+    },
   });
-
-  const submit = (e) => {
-    e.preventDefault();
-    const formData = {
-      to: data.to,
-      method: data.method,
-      sendAt: new Date(data.sendAt).toISOString(),
-      content: data.content,
-    };
-    axios.post(url, formData).then((response) => console.log(response));
-  };
-
-  const handle = (e) => {
-    const newData = { ...data };
-    newData[e.target.id] = e.target.value;
-    setData(newData);
-    console.log(newData);
-  };
 
   return (
     <div>
-      <form onSubmit={(e) => submit(e)}>
+      <form onSubmit={formik.handleSubmit}>
         <h1 className="header">Message schedule app</h1>
         <div className="form-group">
           <label>To</label>
-          <input
-            onChange={(e) => handle(e)}
-            id="to"
-            type="text"
-            className="input"
-            value={data.to}
-            name="to"
-            placeholder="Email or Telegram Id"
-            autoComplete="off"
-          />
+          <div>
+            <input
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.to}
+              id="to"
+              type="text"
+              className="input"
+              name="to"
+              placeholder="Email or Telegram Id"
+              autoComplete="off"
+            />
+            {formik.touched.to && formik.errors.to ? (
+              <div className="error">{formik.errors.to}</div>
+            ) : null}
+          </div>
         </div>
         <div className="form-group">
           <label>Method</label>
-          <select
-            onChange={(e) => handle(e)}
-            id="method"
-            value={data.method}
-            name="method"
-            className="input"
-          >
-            <option value="email">Email</option>
-            <option value="telegram">Telegram</option>
-          </select>
+          <div>
+            <select
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.method}
+              id="method"
+              name="method"
+              className="input"
+            >
+              <option value="email">Email</option>
+              <option value="telegram">Telegram</option>
+            </select>
+            {formik.touched.method && formik.errors.method ? (
+              <div className="error">{formik.errors.method}</div>
+            ) : null}
+          </div>
         </div>
         <div className="form-group">
           <label>Send at date</label>
-          <input
-            onChange={(e) => handle(e)}
-            id="sendAt"
-            type="datetime-local"
-            value={data.sendAt}
-            name="sendAt"
-            className="input"
-          />
+          <div>
+            <input
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.sendAt}
+              id="sendAt"
+              type="datetime-local"
+              name="sendAt"
+              className="input"
+            />
+            {formik.touched.sendAt && formik.errors.sendAt ? (
+              <div className="error">{formik.errors.sendAt}</div>
+            ) : null}
+          </div>
         </div>
         <div className="form-group">
           <label>Content</label>
-          <input
-            onChange={(e) => handle(e)}
-            id="content"
-            type="text"
-            className="input"
-            value={data.content}
-            name="content"
-            placeholder="Content"
-            autoComplete="off"
-          />
+          <div>
+            <input
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.content}
+              id="content"
+              type="text"
+              className="input"
+              name="content"
+              placeholder="Content"
+              autoComplete="off"
+            />
+            {formik.touched.content && formik.errors.content ? (
+              <div className="error">{formik.errors.content}</div>
+            ) : null}
+          </div>
         </div>
         <button className="ui-btn">
           <span>Send it</span>
